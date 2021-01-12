1d0c57814ec21b7ce7d1f698729b4190a32665e1
ragavarvd <ragavarvd@gmail.com>
2021-01-12 09:25:11 +0530

    ngx-build implimentation

--- angular-elements/customebuild.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ng build --prod --output-hashing=none && cat dist/angular-elements/runtime.js dist/angular-elements/polyfills.js dist/angular-elements/main.js > preview/angularapp.js
--- angular-elements/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  api : '81132b2d141960bbc1b5d3f0d7641aee',
-  endPoint : 'https://samples.openweathermap.org', 
-  corsServer : 'https://cors-anywhere.herokuapp.com'
-};
